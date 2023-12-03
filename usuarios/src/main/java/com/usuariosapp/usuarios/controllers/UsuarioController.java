d1a1bdb0ee148cda803855092ad64678ab67636b
Manuel <manuel.machado.j.m@gmail.com>
2023-12-03 01:16:05 -0300

    primera actualizacion del proyecto

---
+++ usuarios/src/main/java/com/usuariosapp/usuarios/controllers/UsuarioController.java
@@ -0,0 +1,50 @@
+package com.usuariosapp.usuarios.controllers;
+
+import com.usuariosapp.usuarios.models.Usuario;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UsuarioController {
+    @RequestMapping(value = "usuario/{id}")
+    public Usuario crear(@PathVariable Long id){
+        Usuario usuario = new Usuario();
+        usuario.setId(id);
+        usuario.setNombre("manuel");
+        usuario.setApellido("machado");
+        usuario.setEmail("machado.j.m@gmail.com");
+        usuario.setTelefono("3794862502");
+        return usuario;
+    }
+
+    @RequestMapping(value = "usuario213123")
+    public Usuario editar(){
+        Usuario usuario = new Usuario();
+        usuario.setNombre("manuel");
+        usuario.setApellido("machado");
+        usuario.setEmail("machado.j.m@gmail.com");
+        usuario.setTelefono("3794862502");
+        return usuario;
+    }
+
+    @RequestMapping(value = "usuario123123")
+    public Usuario eliminar(){
+        Usuario usuario = new Usuario();
+        usuario.setNombre("manuel");
+        usuario.setApellido("machado");
+        usuario.setEmail("machado.j.m@gmail.com");
+        usuario.setTelefono("3794862502");
+        return usuario;
+    }
+
+    @RequestMapping(value = "usuario1231")
+    public Usuario buscar(){
+        Usuario usuario = new Usuario();
+        usuario.setNombre("manuel");
+        usuario.setApellido("machado");
+        usuario.setEmail("machado.j.m@gmail.com");
+        usuario.setTelefono("3794862502");
+        return usuario;
+    }
+}
---
+++ usuarios/src/test/java/com/usuariosapp/usuarios/UsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usuariosapp.usuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
